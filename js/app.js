d7ff4951966585e7533055d163cc1332c5799f7c
Rafael Augusto <rafa.2098@gmail.com>
2016-09-11 19:43:25 -0300

    Controllers #2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var App = angular.module('App', []);
 
 App.controller("Controle", ['$scope', function($scope){
-	$scope.name = "Rafael";
+	$scope.name = "Rafael Augusto";
+	$scope.habilitaNome = function(newName){
+		$scope.name = newName;
+	}
+}]);
+
+App.controller("Controle2", ['$scope', function($scope){
+	$scope.name = "Augusto";
 }]);
